5b8c289c4e860a39279078509a572c37ec843982
Yevgen-Kharchenko <yevgen.kharchenko.jd@gmail.com>
2024-05-13 13:46:51 +0300

    DateTime modul

--- src/main/java/com/testTasks/testAssignment/config/ApplicationProperties.java
+++ src/main/java/com/testTasks/testAssignment/config/ApplicationProperties.java
@@ -1,6 +1,7 @@
 package com.testTasks.testAssignment.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -24,6 +25,7 @@ public class ApplicationProperties {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return mapper;
     }
     @Bean
